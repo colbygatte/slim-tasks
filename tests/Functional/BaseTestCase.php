0699fa41d74fb399de30df816c56fb8d8d285b51
colbygatte <colbygatte@gmail.com>
2017-10-02 13:57:12 -0500

    Reformat

--- tests/Functional/BaseTestCase.php
+++ tests/Functional/BaseTestCase.php
@@ -3,9 +3,9 @@
 namespace Tests\Functional;
 
 use Slim\App;
+use Slim\Http\Environment;
 use Slim\Http\Request;
 use Slim\Http\Response;
-use Slim\Http\Environment;
 
 /**
  * This is an example class that shows how you could set up a method that
@@ -25,20 +25,19 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
     /**
      * Process the application given a request method and URI
      *
-     * @param string $requestMethod the request method (e.g. GET, POST, etc.)
-     * @param string $requestUri the request URI
-     * @param array|object|null $requestData the request data
+     * @param string            $requestMethod the request method (e.g. GET, POST, etc.)
+     * @param string            $requestUri    the request URI
+     * @param array|object|null $requestData   the request data
+     *
      * @return \Slim\Http\Response
      */
     public function runApp($requestMethod, $requestUri, $requestData = null)
     {
         // Create a mock environment for testing with
-        $environment = Environment::mock(
-            [
-                'REQUEST_METHOD' => $requestMethod,
-                'REQUEST_URI' => $requestUri
-            ]
-        );
+        $environment = Environment::mock([
+            'REQUEST_METHOD' => $requestMethod,
+            'REQUEST_URI' => $requestUri
+        ]);
 
         // Set up a request object based on the environment
         $request = Request::createFromEnvironment($environment);
@@ -52,21 +51,21 @@ class BaseTestCase extends \PHPUnit_Framework_TestCase
         $response = new Response();
 
         // Use the application settings
-        $settings = require __DIR__ . '/../../src/settings.php';
+        $settings = require __DIR__.'/../../src/settings.php';
 
         // Instantiate the application
         $app = new App($settings);
 
         // Set up dependencies
-        require __DIR__ . '/../../src/dependencies.php';
+        require __DIR__.'/../../src/dependencies.php';
 
         // Register middleware
         if ($this->withMiddleware) {
-            require __DIR__ . '/../../src/middleware.php';
+            require __DIR__.'/../../src/middleware.php';
         }
 
         // Register routes
-        require __DIR__ . '/../../src/routes.php';
+        require __DIR__.'/../../src/routes.php';
 
         // Process the application
         $response = $app->process($request, $response);
